513226741718757667d25f454954c6f1e9e076c6
marialuizaruiz <marialuizaruiz09@gmail.com>
2024-05-07 19:39:40 -0300

    Atualizar

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-            <h1>Página Inicialllll</h1>
+            <h1>Página Inicial</h1>
             <nav>
                 <ul>
                     <li>
